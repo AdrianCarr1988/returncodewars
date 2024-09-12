3cbd6989e61ff2be6dbb3d8ff42df551036d0afe
TheCrazyHorse <102759638+TCHProductions@users.noreply.github.com>
2024-09-12 19:51:24 +1000

    Create multiply.js
    My first code run in two years.

---
+++ 8kyu/multiply.js
@@ -0,0 +1,19 @@
+//12/09/24
+
+//This code does not execute properly. Try to figure out why.
+
+//function multiply(a, b){
+//    a * b
+//  }
+
+
+//Answer
+
+function multiply(a, b){
+    return a * b
+    
+  }
+
+  
+
+  // 'Return' had to be added to the line of code.
